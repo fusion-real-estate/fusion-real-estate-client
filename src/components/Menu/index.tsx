10e79e3fa62bed8a200ffa0ff896dc79a26e964f
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-16 17:07:39 -0300

    :sparkles: Creating open/close logic with React Hooks and TDD

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,28 +1,39 @@
+import { useState } from 'react'
+
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { Whatsapp } from '@styled-icons/boxicons-logos/Whatsapp'
 import { EmailOutline } from '@styled-icons/evaicons-outline/EmailOutline'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
 
 import * as S from './styles'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile aria-label="Logo" />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <Whatsapp aria-label="Whatsapp" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <EmailOutline aria-label="Email" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile aria-label="Logo" />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <Whatsapp aria-label="Whatsapp" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <EmailOutline aria-label="Email" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
