b2b770daf0bc8cb3c22e64a18bb04bd54060e9fa
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-16 14:24:17 -0300

    :rotating_light: Creating tests and small tweaks

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -1,13 +1,43 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import 'jest-styled-components'
 
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Heading />)
+  it('should render a black heading by default', () => {
+    renderWithTheme(<Heading>Fusion</Heading>)
 
-    expect(
-      screen.getByRole('heading', { name: /Heading/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /Fusion/i })).toHaveStyle({
+      color: '#373737'
+    })
+  })
+
+  it('should render a white heading when color is passed', () => {
+    renderWithTheme(<Heading color="white">Fusion</Heading>)
+
+    expect(screen.getByRole('heading', { name: /Fusion/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Fusion</Heading>)
+
+    expect(screen.getByRole('heading', { name: /Fusion/i })).toHaveStyle({
+      'border-left': '0.7rem solid #34ABD0'
+    })
+  })
+
+  it('should render a heading with a line at the bottom', () => {
+    renderWithTheme(<Heading lineBottom>Fusion</Heading>)
+
+    expect(screen.getByRole('heading', { name: /Fusion/i })).toHaveStyleRule(
+      'border',
+      '0.4rem solid #2691B2',
+      {
+        modifier: '::after'
+      }
+    )
   })
 })
