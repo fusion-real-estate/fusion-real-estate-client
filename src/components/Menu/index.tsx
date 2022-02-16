6213a85da7672992b32de5677d488b7c2cc30ed3
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-16 17:38:23 -0300

    :sparkles: Creating styles for the full menu and animations

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -31,6 +31,13 @@ const Menu = () => {
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Sobre</S.MenuLink>
+          <S.MenuLink href="#">Imóveis</S.MenuLink>
+          <S.MenuLink href="#">Faq</S.MenuLink>
+          <S.MenuLink href="#">Contato</S.MenuLink>
+        </S.MenuNav>
       </S.MenuFull>
     </S.Wrapper>
   )
