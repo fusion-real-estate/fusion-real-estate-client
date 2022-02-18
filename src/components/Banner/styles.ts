1a075f2b5d7c5f6ce28d3c6c734d280d66046a30
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-18 12:04:45 -0300

    :sparkles: Creating banner slider structure and tests

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -76,7 +76,7 @@ export const Subtitle = styled.h3`
     margin-bottom: ${theme.spacings.xsmall};
 
     strong {
-      color: ${theme.colors.primary};
+      color: ${theme.colors.secondary};
       font-weight: ${theme.font.bold};
     }
 
