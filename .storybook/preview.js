1572ed5fb2646ac88f0f03739fa6e30d08bf9166
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-18 16:01:49 -0300

    :sparkles: Creating custom Storybook backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,23 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'fusion-light',
+    values: [
+      {
+        name: 'fusion-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'fusion-dark',
+        value: theme.colors.primary
+      }
+    ]
+  }
+}
+
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -23,7 +23,7 @@ export const IconWrapper = styled.div`
     width: 2.4rem;
     height: 2.4rem;
     cursor: pointer;
-    color: ${theme.colors.white};
+    color: ${theme.colors.primary};
   `}
 `
 
@@ -44,7 +44,7 @@ export const MenuNav = styled.div``
 export const MenuLink = styled.a`
   ${({ theme }) => css`
     position: relative;
-    color: ${theme.colors.white};
+    color: ${theme.colors.primary};
     font-size: ${theme.font.sizes.medium};
     margin: 0.3rem ${theme.spacings.small} 0;
     text-decoration: none;
