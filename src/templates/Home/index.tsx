9e010900acfff57c032e748f77372d61970eaf79
Geraldo Luiz <geraldobl58@gmail.com>
2022-02-21 17:25:46 -0300

    :art: Adding the first static components

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'components/Container'
 import Heading from 'components/Heading'
+import Subtitle from 'components/Subtitle'
 import Menu from 'components/Menu'
 
 import * as S from './styles'
@@ -14,24 +15,28 @@ const Home = () => (
       <Heading lineLeft color="black">
         Destaques
       </Heading>
+      <Subtitle>Imóveis em destaques</Subtitle>
     </Container>
 
     <Container>
       <Heading lineLeft color="black">
         Explore
       </Heading>
+      <Subtitle>Faça sua pesquisa por tipo de imóvel</Subtitle>
     </Container>
 
     <Container>
       <Heading lineLeft color="black">
         Recentes
       </Heading>
+      <Subtitle>Imóveis recentes</Subtitle>
     </Container>
 
     <Container>
       <Heading lineLeft color="black">
         Pesquisa por cidade
       </Heading>
+      <Subtitle>Faça sua busca por cidade</Subtitle>
     </Container>
   </section>
 )
