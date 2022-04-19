0b65953c77016e9d1dea6a11ec49d1f5c875f06c
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-19 18:50:17 -0300

    :sparkles: Creating close method with ESC key (useEffect hook)

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
@@ -55,13 +55,23 @@ const settings: SliderSettings = {
 const GallerySlider = ({ items }: GalleryProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
+
   return (
     <S.Wrapper>
       <Slider settings={settings}>
         {items.map((item, index) => (
           <img
             role="button"
-            key={`thumb-${item.src}`}
+            key={`thumb-${index}`}
             src={item.src}
             alt={`Thumb - ${item.label}`}
             onClick={() => setIsOpen(true)}
--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -47,4 +47,21 @@ describe('<Gallery />', () => {
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0 })
   })
+
+  it('should hamdle the close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={mockItems.slice(0, 2)} />
+    )
+    const modal = screen.getByLabelText('modal')
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+
+    fireEvent.keyUp(container, { key: 'Escape' })
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
 })
