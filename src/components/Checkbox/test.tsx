7cefeae26304a8eb1b000ef9fa32364e91949fc9
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-23 16:27:20 -0300

    :sparkles: Making input dynamic

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -4,7 +4,11 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
-  it('should render the heading', () => {
-    renderWithTheme(<Checkbox />)
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
   })
 })
