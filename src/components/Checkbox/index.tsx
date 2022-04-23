4ad46c9f1c761647aac73f696b97366253373e18
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-23 16:07:06 -0300

    :sparkles: Creating static checkbox and label input

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Checkbox = () => (
+  <S.Wrapper>
+    <input id="sao paulo" type="checkbox" />
+    <label htmlFor="sao paulo">São Paulo</label>
+  </S.Wrapper>
+)
+
+export default Checkbox
