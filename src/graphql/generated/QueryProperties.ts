97413dbcbcecaf8acd42f0a390045af744a3bc09
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-29 17:06:35 -0300

    :sparkles: Using Apollo Codegen to generate query types

---
+++ src/graphql/generated/QueryProperties.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: QueryProperties
+// ====================================================
+
+export interface QueryProperties_properties_cover {
+  __typename: "UploadFile";
+  url: string;
+}
+
+export interface QueryProperties_properties {
+  __typename: "Property";
+  name: string;
+  street: string;
+  slug: string;
+  cover: QueryProperties_properties_cover | null;
+  rooms: number;
+  bathrooms: number;
+  garage: number | null;
+  sqt: number | null;
+  price: number | null;
+}
+
+export interface QueryProperties {
+  properties: QueryProperties_properties[];
+}
+
+export interface QueryPropertiesVariables {
+  limit: number;
+}
