360a8df728b462e93826b470d14ad94441446d08
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-29 14:55:04 -0300

    :sparkles: Populating the page with data via getStaticProps (SSG) with revalidate

--- src/pages/properties.tsx
+++ src/pages/properties.tsx
@@ -1,18 +1,53 @@
+import { gql } from '@apollo/client'
+import { initializeApollo } from 'utils/apollo'
+
 import PropertiesTemplate, {
   PropertiesTemplateProps
 } from 'templates/Properties'
 
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import propertiesMock from 'components/CardSlider/mock'
 
 export default function PropertyPage(props: PropertiesTemplateProps) {
   return <PropertiesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryProperties {
+        properties {
+          name
+          slug
+          cover {
+            url
+          }
+          rooms
+          bathrooms
+          garage
+          sqt
+          price
+        }
+      }
+    `
+  })
+
   return {
     props: {
-      properties: propertiesMock,
+      revalidate: 60,
+      properties: data.properties.map((item) => ({
+        title: item.name,
+        img: item.cover.url,
+        beds: item.bathrooms,
+        bath: item.rooms,
+        garage: item.garage,
+        sqt: item.sqt,
+        price: new Intl.NumberFormat('pt', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(item.price)
+      })),
       filterItems: filterItemsMock
     }
   }
