f01e5d1ec5f93b2028b44b22bb2bac5b545c8cc2
Geraldo Luiz <geraldobl58@gmail.com>
2022-05-09 18:37:26 -0300

    :hammer: Refactoring components

--- src/components/Card/stories.tsx
+++ src/components/Card/stories.tsx
@@ -8,13 +8,13 @@ export default {
     title: 'New Apartment Nice Wiew',
     slug: 'luxury-family-loft-by-victoria-park',
     address: '4 W 21st St Flatiron District, New York, NY',
-    beds: '4',
-    bath: '1',
-    garage: '1',
-    sqt: '150',
+    beds: 4,
+    bath: 1,
+    garage: 1,
+    sqt: 150,
     img:
       'https://creativelayers.net/themes/houzing-html/images/property/fl1.jpg',
-    price: 'R$ 1200.000,00'
+    price: 1200.0
   },
   argTypes: {
     ribbon: { type: 'string' }
